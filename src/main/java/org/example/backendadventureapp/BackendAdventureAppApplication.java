3ab57f16afd009d30ccf1410780a76680becfab3
SimAbild <abildtrup123@gmail.com>
2026-02-26 15:53:58 +0100

    lavet projekt

---
+++ src/main/java/org/example/backendadventureapp/BackendAdventureAppApplication.java
@@ -0,0 +1,13 @@
+package org.example.backendadventureapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAdventureAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendAdventureAppApplication.class, args);
+    }
+
+}
