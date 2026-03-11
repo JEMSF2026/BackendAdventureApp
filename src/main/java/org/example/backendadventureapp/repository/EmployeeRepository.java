8694add82dc75fcf9170e426a9a460adaf3082a1
SimAbild <abildtrup123@gmail.com>
2026-03-11 10:13:37 +0100

    Lavet ændringer til log-in, lavet controller og service og tests

--- src/main/java/org/example/backendadventureapp/repository/EmployeeRepository.java
+++ src/main/java/org/example/backendadventureapp/repository/EmployeeRepository.java
@@ -3,5 +3,11 @@ package org.example.backendadventureapp.repository;
 import org.example.backendadventureapp.model.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository extends JpaRepository<Employee, String> {
+import java.util.Optional;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+    // Optional bruges fordi der måske ikke findes en medarbejder med den email
+    // Det tvinger os til at håndtere tilfældet eksplicit frem for at risikere NullPointerException
+    Optional<Employee> findByEmail(String email);
 }
