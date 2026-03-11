75612ac97bd556483374561ee9509e560c0f4c7c
Joshua-kea <josv0002@stud.kea.dk>
2026-03-11 17:20:03 +0100

    Kæmpe commit. Har lavet primitive til objekter, har lavet hele reserver-aktivitet funktionen i ReservationService. Har lavet test til funktionen

--- src/main/java/org/example/backendadventureapp/controller/ReservationController.java
+++ src/main/java/org/example/backendadventureapp/controller/ReservationController.java
@@ -4,10 +4,7 @@ import org.example.backendadventureapp.model.Reservation;
 import org.example.backendadventureapp.service.ReservationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:63342")
@@ -21,9 +18,14 @@ public class ReservationController {
     @GetMapping("/{bookingNumber}")
     public Reservation getReservationByBookingNumber(@PathVariable String bookingNumber) {
         Reservation reservation = reservationService.findReservationByBookingnumber(bookingNumber);
-        if(reservationService == null) {
+        if(reservation == null) {
             throw new RuntimeException("No reservation was found for the Bookingnumber: " + bookingNumber);
         }
         return reservation;
+    }
+
+    @PostMapping("/reservation")
+    public Reservation bookReservation(@RequestBody Reservation reservation){
+        return reservationService.createReservation(reservation);
     }
 }
--- src/main/java/org/example/backendadventureapp/service/ReservationService.java
+++ src/main/java/org/example/backendadventureapp/service/ReservationService.java
@@ -1,13 +1,14 @@
 package org.example.backendadventureapp.service;
 
-import org.example.backendadventureapp.model.Customer;
-import org.example.backendadventureapp.model.Reservation;
-import org.example.backendadventureapp.repository.CustomerRepository;
-import org.example.backendadventureapp.repository.ReservationRepository;
+import org.example.backendadventureapp.model.*;
+import org.example.backendadventureapp.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Time;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 @Service
@@ -15,27 +16,95 @@ public class ReservationService {
 
     @Autowired
     private ReservationRepository reservationRepository;
-    private final ReservationRepository reservationRepository;
-    private final CustomerRepository customerRepository;
-
-    public ReservationService(ReservationRepository reservationRepository,
-                              CustomerRepository customerRepository){
-
-        this.reservationRepository = reservationRepository;
-        this.customerRepository = customerRepository;
-    }
-
-    public Reservation createReservation(Reservation reservation){
-        reservation.setBookingNumber(generateBookingNumber());
-        reservation.setDateOfReservation(LocalDateTime.now());
+    @Autowired
+    private ActivityRepository activityRepository;
+    @Autowired
+    private CustomerTypeRepository customerTypeRepository;
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private TimeslotRepository timeslotRepository;
 
     //fetch single reservation by bookingNumber:
     public Reservation findReservationByBookingnumber(String bookingNumber) {
         return reservationRepository.findByBookingNumber(bookingNumber);
-        Customer savedCustomer = customerRepository.save(reservation.getCustomer());
-        reservation.setCustomer(savedCustomer);
+    }
+
+    public Reservation createReservation(Reservation reservation) {
+
+        validateCustomer(reservation);
+
+        Customer customer = reservation.getCustomer();
+        customer = customerRepository.save(customer);
+
+        reservation.setCustomer(customer);
+        reservation.setBookingNumber(generateBookingNumber());
+        reservation.setDateOfReservation(LocalDateTime.now());
+
+        attachReservationToTimeslots(reservation);
+
+        calculatePrice(reservation);
 
         return reservationRepository.save(reservation);
+    }
+
+    private void validateCustomer(Reservation reservation){
+
+        if(reservation.getCustomer() == null){
+            throw new RuntimeException("Kundeoplysninger er påkrævet");
+        }
+
+        Customer customer = reservation.getCustomer();
+
+        if(reservation.getCustomer().getFirstName() == null ||
+                reservation.getCustomer().getLastName() == null ||
+                reservation.getCustomer().getEmail() == null){
+            throw new RuntimeException("Manglende kundeoplsyninger");
+        }
+
+        CustomerType type = customerTypeRepository
+                .findById(customer.getCustomerType().getId())
+                .orElseThrow();
+
+        if(type.getId() == 2){
+            if(customer.getCompanyName() == null || customer.getCompanyName().isBlank() ||
+                    customer.getCvr() == null){
+                throw new RuntimeException("CVR og Virksomhedsnavn påkrævet for virksomheder");
+            }
+        }
+
+        customer.setCustomerType(type);
+    }
+
+    public void attachReservationToTimeslots(Reservation reservation) {
+
+        List<Timeslot> attachedTimeslots = new ArrayList<>();
+
+        for (Timeslot t : reservation.getTimeslots()){
+            Timeslot timeslot = timeslotRepository.findById(t.getId()).orElseThrow();
+
+            if(timeslot.getReservation() != null){
+                throw new RuntimeException("Tidspunktet er booket af en anden kunde.");
+            }
+
+            timeslot.setReservation(reservation);
+            timeslot.setParticipants(t.getParticipants());
+
+            attachedTimeslots.add(timeslot);
+        }
+
+        reservation.setTimeslots(attachedTimeslots);
+    }
+
+    public void calculatePrice(Reservation reservation){
+
+        double totalPrice = 0;
+
+        for(var timeslot : reservation.getTimeslots()){
+            totalPrice += timeslot.getActivity().getPrice();
+        }
+
+        reservation.setPrice(totalPrice);
     }
 
     public String generateBookingNumber(){
