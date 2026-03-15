03b2f85251c6b3c159295d46c2f4409336bb1faa
MU-1995 <jynke58@hotmail.com>
2026-03-15 13:30:40 +0100

    fixet imports

--- src/test/java/org/example/backendadventureapp/controller/ActivityControllerTest.java
+++ src/test/java/org/example/backendadventureapp/controller/ActivityControllerTest.java
@@ -4,7 +4,7 @@ import org.example.backendadventureapp.model.Activity;
 import org.example.backendadventureapp.service.ActivityService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
